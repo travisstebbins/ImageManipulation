46e29ac3e263ec3545c763f17e2077e120348ffa
Travis Stebbins <stebbins.travis@gmail.com>
2015-08-02 02:26:48 -0500

    Implement Gaussian function for accurate Gaussian blur

--- src/imagemanipulation/ImageManipulation.java
+++ src/imagemanipulation/ImageManipulation.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
 
@@ -21,7 +20,7 @@ public class ImageManipulation {
     static BufferedImage postImg = null;
     static int width;
     static int height;
-    static int kernel[][] = {{0,-1,0},{-1,5,-1},{0,-1,0}};
+    static double kernel[][];
     
     //helper objects
     static Scanner kb = new Scanner(System.in);
@@ -34,32 +33,51 @@ public class ImageManipulation {
      */
     public static void main(String[] args) throws IOException {
         try {
-            img = ImageIO.read(new File("pre_images/playground.png"));
+            img = ImageIO.read(new File("pre_images/avocados.png"));
             width = img.getWidth();
             height = img.getHeight();
             postImg = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
         } catch (IOException e) {
         }
+        kernel = generateGaussianKernel(0.84089642, 10);
         convolute(kernel);
         export();
     }
     
     /**
-     * Generates a 2D kernel array based on a given radius and
-     * ratio for the Gaussian blur
+     * Generates a 2D kernel array based on a given standard
+     * deviation and radius
      * 
-     * @param radius Gaussian blur radius
-     * @param ratio Ratio of influence of surrounding pixels
+     * @param stdDev Gaussian function standard deviation
+     * @param radius the blur radius
      * @return 2D kernel array
      */
-    public static int[][] generateKernel(int radius, int ratio) {
-        int[][] kernel = new int[(2 * radius) + 1][(2 * radius) + 1];
+    public static double[][] generateGaussianKernel(double stdDev, int radius) {
+        double[][] kernel = new double[(2 * radius) + 1][(2 * radius) + 1];
+        int origin = (int) (kernel.length / 2.0);
         for(int i = 0; i < kernel.length; i++) {
             for(int j = 0; j < kernel[0].length; j++) {
-                kernel[i][j] = 1;
+                int x = Math.abs(i - origin);
+                int y = Math.abs(j - origin);
+                kernel[i][j] = gaussian(x, y, stdDev);
             }
         }
         return kernel;
+    }
+    
+    /**
+     * Calculates the Gaussian filter kernel value at a specific pixel
+     * based on the pixel's position and the Gaussian function's
+     * standard deviation
+     * 
+     * @param x the pixel's horizontal position within the kernel
+     * @param y the pixel's vertical position within the kernel
+     * @param stdDev the Gaussian function's standard deviation
+     * @return double value for the pixel's Gaussian kernel value
+     */
+    private static double gaussian(int x, int y, double stdDev) {
+        return (1 / (2 * Math.PI * Math.pow(stdDev, 2))) * 
+                Math.pow(Math.E, -((Math.pow(x, 2) + Math.pow(y, 2)) / (2 * Math.pow(stdDev, 2))));
     }
     
     /**
@@ -68,27 +86,22 @@ public class ImageManipulation {
      * @param kernel the kernel that is convoluted over the image; different kernels
      * produce different effects (i.e. gaussian blur, sharpen, etc.)
      */
-    public static void convolute(int[][] kernel)
+    public static void convolute(double[][] kernel)
     {
         for(int i = (int) Math.floor(kernel.length / 2); i < width - (int) Math.floor(kernel.length / 2); i++) {
             for(int j = (int) Math.floor(kernel[0].length / 2); j < height - (int) Math.floor(kernel[0].length / 2); j++) {
-                int counter = 0;
                 int rAccum = 0;
                 int gAccum = 0;
                 int bAccum = 0;
                 for(int x = -(int) Math.floor(kernel.length / 2); x < (int) Math.floor(kernel.length / 2); x++) {
                     for(int y = 0 - (int) Math.floor(kernel[0].length / 2); y < (int) Math.floor(kernel[0].length / 2); y++) {
-                        counter += kernel[x + (int) Math.floor(kernel.length / 2)][y + (int) Math.floor(kernel[0].length / 2)];
                         Color color = new Color(img.getRGB(i + x, j + y));
                         rAccum += kernel[x + (int) Math.floor(kernel.length / 2)][y + (int) Math.floor(kernel[0].length / 2)] * color.getRed();
                         gAccum += kernel[x + (int) Math.floor(kernel.length / 2)][y + (int) Math.floor(kernel[0].length / 2)] * color.getGreen();
                         bAccum += kernel[x + (int) Math.floor(kernel.length / 2)][y + (int) Math.floor(kernel[0].length / 2)] * color.getBlue();
                     }
                 }
-                int rAvg = (int) Math.ceil(rAccum / counter);
-                int gAvg = (int) Math.ceil(gAccum / counter);
-                int bAvg = (int) Math.ceil(bAccum / counter);
-                postImg.setRGB(i, j, toRGB(rAvg, gAvg, bAvg));
+                postImg.setRGB(i, j, toRGB(rAccum, gAccum, bAccum));
             }
         }
     }
